a362ddf89dbf88aceab8d2b19f2fa39c4e2daca7
Anthony <59890724+narlock@users.noreply.github.com>
2023-12-02 08:08:59 -0600

    initial commit

---
+++ rogue/src/test/java/com/narlock/rogue/RogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narlock.rogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
